e927c86f3f6750017d3c5529e898e268da7729a4
n.herron <nherron@nherrons-MacBook-Pro.local>
2017-04-26 11:05:16 -0500

    add italian.js file

---
+++ italian-NH.js
@@ -0,0 +1,28 @@
+var uzbOutput = document.getElementById('output');
+var slogan2 = (function (originalslogan) {
+	var italWords = {"make":"Rendere", "again":"ancora", "great":"di nuovo", "christmas":"Natale"}
+
+		originalslogan.translateToItalian = function(value) {
+
+			var rev = value.split(" ");
+			var keyNames = Object.keys(italWords);
+				var lookup = {};
+				for (var j in rev) {
+				    lookup[rev[j]] = rev[j];
+				}
+				for (var i in keyNames) {
+				    if (typeof lookup[keyNames[i]] != 'undefined') {
+				    	if (keyNames[i] === "make") {
+				    		uzbOutput.innerHTML += `<h1>${italWords.make}</h1>`;
+				    	} else if (keyNames[i] === "again") {
+				    		uzbOutput.innerHTML += `<h1>${italWords.again}</h1>`;
+				    	} else if (keyNames[i] === "great") {
+				    		uzbOutput.innerHTML += `<h1>${italWords.great}</h1>`;
+				    	} else if (keyNames[i] === "christmas") {
+				    		uzbOutput.innerHTML += `<h1>${italWords.christmas}</h1>`;
+				    	}
+				    }
+				}
+	  	}
+return slogan2;
+})(slogan1);
